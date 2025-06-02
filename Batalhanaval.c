0db1d7aa822a1de06a99769104169c01ccc53a49
Vitor Carlos da Silva <vitorcarlossilva62@gmail.com>
2025-06-02 21:34:46 +0000

    nivel aventureiro

--- Batalhanaval.c
+++ Batalhanaval.c
@@ -10,15 +10,25 @@ int main() {
         }
     }
 
-    // Navio horizontal de tamanho 3 (linha 2, colunas B-D => [1][1] a [1][3])
-    for (int j = 1; j <= 3; j++) {
-        tabuleiro[1][j] = 3;
+    // Navio horizontal de tamanho 3 (linha 1, colunas 1-3 => [0][0] a [0][2])
+    for (int j = 2; j <= 4; j++) {
+        tabuleiro[0][j] = 3;
     }
 
     // Navio vertical de tamanho 3 (coluna H, linhas 5-7 => [4][7] a [6][7])
     for (int i = 4; i <= 6; i++) {
         tabuleiro[i][7] = 3;
     }
+    // Navio diagonal principal (de [0][0] a [2][2])
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[i][i] = 3;
+    }
+
+    // Navio diagonal secundária (de [0][9] a [2][7])
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[i][9 - i] = 3;
+    }
+    
 
     // Imprimir cabeçalho das colunas (A-J)
     printf("   ");
